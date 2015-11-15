017abcf4ece05d26b86856e4f141702bc16e3449
thebixby <Brandi@Heathcliff.local>
2015-11-15 15:19:35 -0600

    formElement changes

--- script.js
+++ script.js
@@ -1,32 +1,50 @@
 $(function() {
+
+	function Employee(firstName, lastName, employeeNumber, empTitle, lastReview, salary) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.employeeNumber = employeeNumber;
+		this.empTitle = empTitle;
+		this.lastReview = lastReview;
+		this.salary = salary;
+
+	}
+
+
 	function getData($formElement) {
 		// just return some bullshit:
-		var firstName = 'Some';
-		var lastName = 'Person';
-		var employeenumber = 2015;
-		var title = 'Time Lord';
-		var lastreview = 5;
-		var salary = 100000;
-		return {
-			firstName: firstName,
-			lastName: lastName,
-			employeenumber: employeenumber,
-			title: title,
-			lastreview: lastreview,
-			salary: salary
-		};
+		var firstName = $formElement[0];
+		var lastName = $formElement[1];
+		var employeeNumber = $formElement[2];
+		var empTitle = $formElement[3];
+		var lastReview = $formElement[4];
+		var salary = $formElement[5];
+
+
+		var employee = new Employee(firstName, lastName, employeeNumber, empTitle, lastReview, salary);
+
+		return employee;
 
 	}
 
+	$('form#myForm').on('submit', function(event) {
+		event.preventDefault();
+		console.log(event);
+		var formData = $(this).serializeArray();
+		console.log(formData);
+		var results = getData(formData);
+		console.log(results);
 
-	document.getElementById('submit').addEventListener('click', itme);
+	});
 
-	function itme() {
-		console.log("It Me!");
-	}
 
-	document.getElementById('deleteEmp').addEventListener('click', girlbye);
-	function girlbye() {
-		console.log('Good day to you, Felicia');
-	}
+	// var form = document.getElementById('myForm').elements;
+
+	// // var submitted = false;
+
+	// document.getElementById('deleteEmp').addEventListener('click', girlbye);
+
+	// function girlbye() {
+	// 	console.log('Good day to you, Felicia');
+	// }
 });
