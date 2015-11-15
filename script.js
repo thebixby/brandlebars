307a247f6f748b19d7b88259deeabd4c5293297c
thebixby <Brandi@Heathcliff.local>
2015-11-15 13:08:45 -0600

    Update javascript buttons

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-function getData($formElement) {
-// just return some bullshit:
-var firstName = 'Some';
-var lastName = 'Person';
-var employeenumber = 2015;
-var title = 'Time Lord'
-var lastreview = 5
-var salary = 100000
-return { firstName: firstName, lastName: lastName, employeenumber: employeenumber, title: title, lastreview: lastreview, salary: salary };
+$(function() {
+	function getData($formElement) {
+		// just return some bullshit:
+		var firstName = 'Some';
+		var lastName = 'Person';
+		var employeenumber = 2015;
+		var title = 'Time Lord';
+		var lastreview = 5;
+		var salary = 100000;
+		return {
+			firstName: firstName,
+			lastName: lastName,
+			employeenumber: employeenumber,
+			title: title,
+			lastreview: lastreview,
+			salary: salary
+		};
 
-}
+	}
 
 
-document.getElementById('submit').addEventListener('click', console.log('It Me!'));
+	document.getElementById('submit').addEventListener('click', itme);
+
+	function itme() {
+		console.log("It Me!");
+	}
+
+	document.getElementById('deleteEmp').addEventListener('click', girlbye);
+	function girlbye() {
+		console.log('Good day to you, Felicia');
+	}
+});
